92440f99979129d20c23a6e861436e5151f4378f
Otavio Xavier <otavioxaviermarinho@gmail.com>
2024-08-26 09:30:31 -0300

    :tada: initial commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,92 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { StyleSheet, View, Text, TouchableHighlight } from "react-native";
+import { Image } from "expo-image";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { ThemedText } from "@/components/ThemedText";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ArrowRight } from "lucide-react-native";
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
+    <SafeAreaView style={styles.container}>
+      <View>
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+          transition={1000}
+          contentFit="cover"
+          style={styles.imageLogo}
+          source={require("@/assets/images/logo.svg")}
         />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
+        <Text style={styles.titulo}>CONTA'S</Text>
+        <ThemedText type="default" style={styles.subtitulo}>
+          Cuidar do seu dinheiro não é tão difícil
         </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+      </View>
+      <TouchableHighlight style={styles.button}>
+        <View style={styles.buttonContainer}>
+          <Text style={styles.textButton}>Começar</Text>
+          <View style={styles.arrowContainer}>
+            <ArrowRight color={"#FCFCFC"} size={36} />
+          </View>
+        </View>
+      </TouchableHighlight>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    flex: 1,
+    backgroundColor: "#111418",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  imageLogo: {
+    width: 200,
+    height: 200,
+    objectFit: "cover",
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  titulo: {
+    fontFamily: "Jomhuria",
+    fontSize: 84,
+    marginVertical: -20,
+    textAlign: "center",
+    color: "#FCFCFC",
+  },
+  subtitulo: {
+    textAlign: "center",
+    width: 190,
+    fontFamily: "Poppins",
+  },
+  textButton: {
+    color: "#111418",
+    textAlign: "center",
+    flex: 1,
+    fontSize: 20,
+    fontFamily: "Poppins",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    flex: 1,
+    justifyContent: "space-between",
+    paddingHorizontal: 15,
+  },
+  button: {
+    width: 300,
+    height: 80,
+    backgroundColor: "#BAFFD8",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    borderRadius: 45,
+    position: "absolute",
+    bottom: 40,
+  },
+  arrowContainer: {
+    backgroundColor: "#111418",
+    borderRadius: 100,
+    width: 64,
+    height: 64,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,37 +1,38 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback } from "react";
+import { useFonts } from "expo-font";
+import { View } from "react-native";
+import { useColorScheme } from "@/hooks/useColorScheme";
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+
+  const [fontsLoaded] = useFonts({
+    SpaceMono: require("@/assets/fonts/SpaceMono-Regular.ttf"),
+    Poppins: require("@/assets/fonts/Poppins-Regular.ttf"),
+    Jomhuria: require("@/assets/fonts/Jomhuria-Regular.ttf"),
   });
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
     }
-  }, [loaded]);
+  }, [fontsLoaded]);
 
-  if (!loaded) {
+  if (!fontsLoaded) {
     return null;
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <View onLayout={onLayoutRootView} style={{ flex: 1 }}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+      </View>
     </ThemeProvider>
   );
 }
