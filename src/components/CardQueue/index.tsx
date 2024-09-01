dff133630b0ac848ae63275beb96e53ee271f3f4
Otavio Xavier <otavioxaviermarinho@gmail.com>
2024-09-01 11:11:17 -0300

    git commit -m "✨ feat: Add add card dot component into cardQueue Component"

--- src/components/CardQueue/index.tsx
+++ src/components/CardQueue/index.tsx
@@ -3,6 +3,7 @@ import Card from '../Card';
 import { styles } from './styles';
 import type { QueueProps } from './queue';
 import { useEffect, useRef } from 'react';
+import CardDot from '../CardDot';
 
 export default function CardQueue({ cards, card, onChange }: QueueProps) {
   const flatListRef = useRef<FlatList>(null);
@@ -13,17 +14,34 @@ export default function CardQueue({ cards, card, onChange }: QueueProps) {
       flatListRef.current.scrollToIndex({ index, animated: true });
     }
   }, [card, cards]);
-  
+
   return (
-    <FlatList
-      ref={flatListRef}
-      data={cards}
-      keyExtractor={(item) => item.id.toString()}
-      renderItem={({ item }) => <Card card={item} selected={item === card} onPress={() => onChange(item)} />}
-      horizontal
-      showsHorizontalScrollIndicator={false}
-      style={styles.list}
-      contentContainerStyle={styles.content}
-    />
+    <>
+      <FlatList
+        ref={flatListRef}
+        data={cards}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => <Card card={item} selected={item === card} onPress={() => onChange(item)} />}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        style={styles.list}
+        contentContainerStyle={styles.content}
+      />
+      <FlatList
+        data={cards}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <CardDot
+            selected={item === card}
+            onPress={() => onChange(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        style={styles.dotList}
+        contentContainerStyle={styles.dotContent}
+      />
+    </>
+
   )
 }
--- src/components/CardQueue/styles.ts
+++ src/components/CardQueue/styles.ts
@@ -1,11 +1,20 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-  list: {
-    maxHeight: 220,
-  },
-  content: {
-    gap: 16,
-    paddingLeft: 32
-  }
-})
+	list: {
+		maxHeight: 220,
+	},
+	content: {
+		gap: 16,
+		paddingLeft: 32,
+	},
+	dotContent: {
+		flex: 1,
+		flexDirection: "row",
+		height: 20,
+		alignItems: "center",
+		justifyContent: "center",
+		gap: 8,
+	},
+	dotList: {},
+});
