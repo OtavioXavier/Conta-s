e79b424dd80e8413501b87fe30f16aaa4c31be68
Otavio Xavier <otavioxaviermarinho@gmail.com>
2024-09-01 08:52:44 -0300

    git commit -m "🗃️ raw: Add mock data array to simulate database response"

---
+++ src/utils/Cards.ts
@@ -0,0 +1,26 @@
+export const CARDS: Card[] = [
+	{
+		id: 0,
+		name: "Otavio X S M Santos",
+		validate: "06/2028",
+		lastNumbers: "4589",
+		flag: "@/src/assets/images/MasterCard.svg",
+		color: "#1C2128",
+	},
+	{
+		id: 1,
+		name: "Jane Doe",
+		validate: "12/2025",
+		lastNumbers: "1234",
+		flag: "@/src/assets/images/MasterCard.svg",
+		color: "#2C3E50",
+	},
+	{
+		id: 2,
+		name: "John Smith",
+		validate: "11/2024",
+		lastNumbers: "9876",
+		flag: "@/src/assets/images/MasterCard.svg",
+		color: "#34495E",
+	},
+];
