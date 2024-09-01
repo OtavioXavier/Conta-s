cfc01e3acb549ef302d786fa8d0df8d028cc3ad9
Otavio Xavier <otavioxaviermarinho@gmail.com>
2024-09-01 08:51:55 -0300

    git commit -m "✨ feat: Add theme object for colors and fonts"

---
+++ src/theme/colors.ts
@@ -0,0 +1,6 @@
+export const colors = {
+  main: '#BAFFD8',
+  black: '#111418',
+  white: '#FCFCFC',
+
+}
---
+++ src/theme/fontFamily.ts
@@ -0,0 +1,6 @@
+export const fontFamily = {
+  regular: "Poppins_400Regular",
+  medium: "Poppins_500Medium",
+  bold: "Poppins_600SemiBold",
+  black: "Poppins_800ExtraBold",
+}
