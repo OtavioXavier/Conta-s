6c6deec84fa1b308b39a4cafaefc076dff4c604f
Otavio Xavier <otavioxaviermarinho@gmail.com>
2024-09-01 08:55:30 -0300

    git commit -m "✨ feat: Add index file for wallet screen"

--- src/app/(tabs)/wallet/index.tsx
+++ src/app/(tabs)/wallet/index.tsx
@@ -1,14 +1,17 @@
 import Header from '@/src/components/layout/Header';
-import CardQueue from '@/src/components/wallet/CardQueue';
+import CardQueue from '@/src/components/CardQueue';
 import { styles } from '@/src/utils/style';
-import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { CARDS } from '@/src/utils/Cards';
+import { useState } from 'react';
+
 
 export default function WalletScreen() {
+	const [card, setCard] = useState<Card>(CARDS[0]);
 	return (
 		<SafeAreaView style={styles.container}>
-			<Header title='Carteira'/>
-			 <CardQueue />
+			<Header title='Carteira' />
+			<CardQueue cards={CARDS} card={card} onChange={setCard}/>
 		</SafeAreaView>
 	);
 }
