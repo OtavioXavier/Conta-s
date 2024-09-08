cfedf770f199e216553949fe873c9b929e34816a
Otavio Xavier <otavioxaviermarinho@gmail.com>
2024-09-07 23:15:11 -0300

    💄 feat: new colors (blue and black 50%)

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -1,6 +1,12 @@
 export const colors = {
-  main: '#BAFFD8',
-  black: '#111418',
-  white: '#FCFCFC',
-
-}
+	main: "#BAFFD8",
+	black: {
+		100: "#111418",
+		75: "rgba(17, 20, 24, 0.75)",
+	},
+	white: "#FCFCFC",
+	blue: {
+		100: "#96DDED",
+		50: "rgba(150, 221, 237, 0.35)",
+	},
+};
