f31300ac2c05f59f9a1e72b5898783d7984090b0
Otavio Xavier <otavioxaviermarinho@gmail.com>
2024-09-07 23:17:20 -0300

    :sparkles: bottomSheet logic

--- src/app/(tabs)/wallet/_layout.tsx
+++ src/app/(tabs)/wallet/_layout.tsx
@@ -1,13 +1,31 @@
+import AddCard from "@/components/AddCard";
+import Header from "@/components/layout/Header";
+import type BottomSheet  from "@gorhom/bottom-sheet";
 import { Stack } from "expo-router";
+import { useRef } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function WalletLayout() {
+	const bottomSheetRef = useRef<BottomSheet>(null);
+
+	const onPressPlus = () => {
+		handleOpenAddCard()
+	};
+
+	const handleOpenAddCard = () => bottomSheetRef.current?.expand();
+	const handleCloseAddCard = () => bottomSheetRef.current?.snapToIndex(0);
+
 	return (
-		<Stack
-			screenOptions={{
-				headerShown: false,
-			}}
-		>
-			<Stack.Screen name="index" />
-		</Stack>
+		<SafeAreaView style={{ flex: 1 }}>
+			<Header title="Carteira" onPressPlus={onPressPlus} />
+			<Stack
+				screenOptions={{
+					headerShown: false,
+				}}
+			>
+				<Stack.Screen name="index" />
+			</Stack>
+			<AddCard ref={bottomSheetRef} onClose={handleCloseAddCard}/>
+		</SafeAreaView>
 	);
 }
